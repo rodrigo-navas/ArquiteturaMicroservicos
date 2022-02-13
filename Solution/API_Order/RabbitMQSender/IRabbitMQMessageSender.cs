9a818a2dd84ade94457d77a2247f38e86d79fb78
Rodrigo Navas <rodrigo-navas@outlook.com>
2022-02-13 19:21:30 -0300

    Adicionando Consumer do Rabbit

---
+++ Solution/API_Order/RabbitMQSender/IRabbitMQMessageSender.cs
@@ -0,0 +1,9 @@
+﻿using MessageBus;
+
+namespace API_Order.RabbitMQSender
+{
+    public interface IRabbitMQMessageSender
+    {
+        void SendMessage(BaseMessage baseMessage, string queueName);
+    }
+}
---
+++ Solution/API_Order/Repository/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using API_Order.Model;
+using System.Threading.Tasks;
+
+namespace API_Order.Repository
+{
+    public interface IOrderRepository
+    {
+        Task<bool> AddOrder(OrderHeader header);
+        Task UpdateOrderPaymentStatus(long orderHeaderId, bool paid);
+    }
+}
